df948d145640a505ca12db15d2ed462eaa477433
Railson Mendes <railsonmds@hotmail.com>
2023-07-28 19:18:59 -0300

    aula 11 finalizada com pedência

---
+++ aula 11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 2
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60){//condição simples
+     console.log(`você ultrapassou a velociade permitida. MULTADO!`)
+}
+console.log(`diriga sempre usando cinto de seguranca`)
---
+++ aula 11/ex009.js
@@ -0,0 +1,9 @@
+var país = `Brasil`
+
+console.log(`vivendo em ${país}`)
+
+if (país == `Brasil`){
+    console.log(`você é Brasileiro`)
+}else{
+    console.log(`você é Estrangeiro!`)
+}
