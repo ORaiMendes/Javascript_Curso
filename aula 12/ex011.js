18868c98a98a7041cfedaec297bc828b023cd1c6
Railson Mendes <railsonmds@hotmail.com>
2023-07-30 21:41:01 -0300

    Aula 12 finalizada( condições)

---
+++ aula 12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 68
+console.log (`você tem ${idade} anos.`)
+if (idade < 16) {
+    console.log("não vota")
+} else if ( idade < 18 || idade>65 ) {
+    console.log('voto opcinal')       
+} else{
+    console.log('voto obrigatorio')
+}
+  
